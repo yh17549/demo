2d1b3f27db935ae3df174f6c1c5ae1de76620730
yh17549 <1754921231@qq.com>
2018-09-22 18:49:14 +0800

    init

---
+++ src/main/java/io/github/yh17549/demo/App.java
@@ -0,0 +1,14 @@
+package io.github.yh17549.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ){
+        System.out.println("hello world");
+
+    }
+
+}
