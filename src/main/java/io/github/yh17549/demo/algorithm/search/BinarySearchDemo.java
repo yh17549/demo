aab6720f62baef4a243951f7e1d45f6734cbc702
yh17549 <1754921231@qq.com>
2019-02-12 14:12:03 +0800

    增加注释

--- src/main/java/io/github/yh17549/demo/algorithm/search/BinarySearchDemo.java
+++ src/main/java/io/github/yh17549/demo/algorithm/search/BinarySearchDemo.java
@@ -12,20 +12,27 @@ public class BinarySearchDemo {
     public static void main(String[] args) {
         /*
         二分查找
+        前提: 元素需要有序排列
         时间复杂度: O(log n)
          */
 
         Random random = new Random();
 
-        int[] intArrays = generateOrderedIntArray(random.nextInt(10) + 20);
+        int[] intArrays = BinarySearchDemo.generateOrderedIntArray(random.nextInt(10) + 20);
         System.out.println("array:" + Arrays.toString(intArrays));
 
         int key = random.nextInt(100);
         System.out.println("key:" + key);
-        int index = binarySearch(intArrays, key);
+        int index = BinarySearchDemo.binarySearch(intArrays, key);
         System.out.println("index:" + index);
     }
 
+    /**
+     *
+     * 生成有序的数组
+     * @param arrayLength 数组长度
+     * @return
+     */
     private static int[] generateOrderedIntArray(int arrayLength) {
         int[] intArray = new int[arrayLength];
 
@@ -33,6 +40,7 @@ public class BinarySearchDemo {
 
         int j = 0;
         for (int i = 0; i <= arrayLength - 1; i++) {
+            // 生成 1~9 的随机数
             j = j + random.nextInt(9) + 1;
             intArray[i] = j;
         }
